8da339b4577695287f5b0765ea86c6bd0768606e
Oliver Kwun-Morfitt <oliverkwunmorfitt@gmail.com>
2026-03-26 12:52:42 -0400

    finished training script (TODO: Traing the model)

--- train.py
+++ train.py
@@ -1,7 +1,12 @@
-
-
-from transformers import AutoModelForTokenClassification, AutoTokenizer
 import json
+import numpy as np
+from seqeval import metrics as seqeval_metrics
+from datasets import Dataset
+from transformers import (AutoModelForTokenClassification, 
+                          AutoTokenizer, 
+                          TrainingArguments, 
+                          Trainer, 
+                          DataCollatorForTokenClassification)
 
 label_list = ["O", "B-Skill", "I-Skill", "B-Knowledge", "I-Knowledge"]
 
@@ -58,3 +63,90 @@ dev_data = load_skillspan('./json/dev.json')
 #         print("---")
 #         break
 
+def tokenize_and_align(example, tokenizer, label2id):
+    tokenized = tokenizer(
+        example['tokens'],
+        is_split_into_words=True,
+        truncation=True,
+        padding=False
+    )
+
+    labels = []
+    previous_word_idx = None
+    for word_idx in tokenized.word_ids():
+        if word_idx is None:
+            labels.append(-100)
+        elif word_idx != previous_word_idx:
+            labels.append(label2id[example["tags"][word_idx]])
+        else:
+            labels.append(-100)
+        previous_word_idx = word_idx
+    
+    tokenized["labels"] = labels
+    return tokenized
+
+label_list = ["O", "B-Skill", "I-Skill", "B-Knowledge", "I-Knowledge"]
+label2id = {l: i for i, l in enumerate(label_list)}
+id2label = {i: l for i, l in enumerate(label_list)}
+
+train_tokenized = [tokenize_and_align(ex, tokenizer, label2id) for ex in train_data]
+dev_tokenized = [tokenize_and_align(ex, tokenizer, label2id) for ex in dev_data]
+test_tokenized = [tokenize_and_align(ex, tokenizer, label2id) for ex in test_data]
+
+train_dataset = Dataset.from_list(train_tokenized)
+dev_dataset = Dataset.from_list(dev_tokenized)
+test_dataset = Dataset.from_list(test_tokenized)
+
+data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
+
+training_args = TrainingArguments(
+    output_dir = './skillgraph-ner',
+    num_train_epochs=10,
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=4,
+    gradient_accumulation_steps=4,
+    learning_rate=2e-5,
+    weight_decay=0.01,
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    metric_for_best_model="f1"
+)
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    preds = np.argmax(logits, axis=-1)
+
+    true_labels = []
+    true_preds = []
+
+    for pred_seq, label_seq in zip(preds, labels):
+        seq_preds = []
+        seq_labels = []
+        for p, l in zip(pred_seq, label_seq):
+            if l == -100:
+                continue
+
+            seq_preds.append(id2label[int(p)])
+            seq_labels.append(id2label[int(l)])
+
+        true_labels.append(seq_labels)
+        true_preds.append(seq_preds)
+
+    return {
+        "precision": seqeval_metrics.precision_score(true_labels, true_preds),
+        "recall": seqeval_metrics.recall_score(true_labels, true_preds),
+        "f1": seqeval_metrics.f1_score(true_labels, true_preds),
+    }
+
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=dev_dataset,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
